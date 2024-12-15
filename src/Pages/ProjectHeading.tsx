be64561ff3efe89c7f50ee42635de83767238c67
Jason Goelzer <jasongoelzer23@gmail.com>
2024-12-15 14:48:06 -0600

    Reduce project heading text size

--- src/Pages/ProjectHeading.tsx
+++ src/Pages/ProjectHeading.tsx
@@ -4,7 +4,7 @@ const ProjectHeading: React.FC = () => {
   return (
     <div className="absolute w-full top-0">
       <div className="container py-3 flex flex-row justify-start">
-        <p className="">JASON GOELZER | SOFTWARE ENGINEER</p>
+        <p className="text-sm">JASON GOELZER | SOFTWARE ENGINEER</p>
       </div>
     </div>
   )
